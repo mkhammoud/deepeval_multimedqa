eb2415d6c3a18b1580ab0d0e517b0dc783196e05
mkhammoud <moh.k.hammoud@gmail.com>
2024-07-15 23:14:20 +0300

    Initial Commit

---
+++ deepeval/benchmarks/multi_med_qa_mcq/template.py
@@ -0,0 +1,49 @@
+
+
+from typing import List, Optional
+
+
+class MultiMedQAMCQTemplate:
+
+    # Most of this template was taken from MMLU Github Repo
+    # The output confinement is a novel addition, since the original code
+    # outputted log_probabilties for each answer choice
+
+    @staticmethod
+    def generate_output(
+        input: str, train_set: object, task:str , n_shots: int
+    ):
+        prompt = "<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are being assessed on your knowledge of medical subjects to evaluate your understanding and reasoning capabilities in this field. Below, you will find multiple-choice questions related to medicine and biology. Each question has four options, and you must choose only the single correct one, represented by its corresponding letter. You should only reply with the correct Letter without any additional explanation or words, only one single letter. <|eot_id|>\n\n"
+        
+        for i in range(n_shots):
+            prompt += MultiMedQAMCQTemplate.format_question(train_set[i])
+            #print(f"FORMAT QUESTION OF N SHOTS: {i}",MultiMedQAMCQTemplate.format_question(train_set[i]))
+        prompt += input
+
+        # define ouptut confinement
+        #prompt += "Output 'A', 'B', 'C', or 'D'. Full answer not needed."
+        #print(prompt)
+        #print(input)
+        return prompt
+
+    @staticmethod
+    def format_question(data: dict, include_answer: bool = True,  ):
+        prompt = "<|start_header_id|>user<|end_header_id|>"
+        context_data = data["data"].get("Context", None)
+
+        prompt += data["data"]["Question"]
+
+
+        if context_data:
+            context_paragraph = '\n\n'.join(context_data)
+            prompt += "\n\nMake use of the following context to formulate your final answer: " + context_paragraph + "\n\n"
+            
+        choices = data["data"]["Options"]
+        prompt+="\n\nOptions:"
+        for key, value in choices.items():
+            prompt += f"\n{key}:{value}"
+        prompt += "<|eot_id|>\n<|start_header_id|>assistant<|end_header_id|>"
+        if include_answer:
+            prompt += "{}".format(data["data"]["Correct Option"]) +"<|eot_id|>"
+        return prompt
+
